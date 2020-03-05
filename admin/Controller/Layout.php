1f1d3ae6e2b346b16fb37a883d6545162bd4846f
gladysbroce <44674409+gladysbroce@users.noreply.github.com>
2020-03-05 23:35:44 +0900

    Add files via upload

--- admin/Controller/Layout.php
+++ admin/Controller/Layout.php
@@ -1,9 +1,17 @@
 <?php
 class Layout extends System {
 	public function __construct() {
+		parent::__construct();
+		$this->_seats = new Seats();
 	}
 	
 	public function index()	{
 		$this->setTemplate('View/Layout/index.phtml');
+	}
+	
+	public function update($args = array()) {
+		//header('Content-Type: application/json; charset=utf-8');
+		$restaurant_id = 1;
+		$this->_seats->updateSeats($restaurant_id);
 	}
 }
