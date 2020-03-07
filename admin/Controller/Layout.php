a519119846449201691a0c21ddf7260f3c6ef5d4
gladysbroce <44674409+gladysbroce@users.noreply.github.com>
2020-03-08 01:42:13 +0900

    Add files via upload

--- admin/Controller/Layout.php
+++ admin/Controller/Layout.php
@@ -1,17 +1,32 @@
 <?php
 class Layout extends System {
 	public function __construct() {
-		parent::__construct();
 		$this->_seats = new Seats();
 	}
-	
 	public function index()	{
+		$restaurant_id = 1;
+		$floors = $this->_seats->getFloors($restaurant_id);
+		foreach ($floors as $floor) {
+			$this->floors[$floor['floor_id']] = $this->_seats->getSeats($restaurant_id, $floor['floor_id']);
+		}
 		$this->setTemplate('View/Layout/index.phtml');
 	}
-	
-	public function update($args = array()) {
-		//header('Content-Type: application/json; charset=utf-8');
-		$restaurant_id = 1;
-		$this->_seats->updateSeats($restaurant_id);
+	public function add() {
+		$restaurant_id = isset($_POST["restaurant_id"]) ? $_POST["restaurant_id"] : "";
+		$seats = isset($_POST["seats"]) ? $_POST["seats"] : "";
+		if ($restaurant_id && $seats) {
+		    foreach ($seats as $seat) {
+		    	$this->_seats->addSeat($restaurant_id, $seat["floor_id"], $seat["row_no"], $seat["col_no"]);
+		    }
+		}
+	}
+	public function delete() {
+		$restaurant_id = isset($_POST["restaurant_id"]) ? $_POST["restaurant_id"] : "";
+		$seats = isset($_POST["seats"]) ? $_POST["seats"] : "";
+		if ($restaurant_id && $seats) {
+		    foreach ($seats as $seat) {
+		    	$this->_seats->deleteSeat($restaurant_id, $seat["floor_id"], $seat["row_no"], $seat["col_no"]);
+		    }
+		}
 	}
 }
--- admin/Controller/Status.php
+++ admin/Controller/Status.php
@@ -1,9 +1,14 @@
 <?php
 class Status extends System {
 	public function __construct() {
+		$this->_seats = new Seats();
 	}
-	
 	public function index()	{
-		$this->setTemplate( 'View/Status/index.phtml' );
+		$restaurant_id = 1;
+		$floors = $this->_seats->getFloors($restaurant_id);
+		foreach ($floors as $floor) {
+			$this->floors[$floor['floor_id']] = $this->_seats->getSeats($restaurant_id, $floor['floor_id']);
+		}
+		$this->setTemplate('View/Status/index.phtml');
 	}
 }
