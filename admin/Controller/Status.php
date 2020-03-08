092f317cc30379a6767ad237dc0326576ebd8a8f
gladysbroce <44674409+gladysbroce@users.noreply.github.com>
2020-03-08 16:35:42 +0900

    Add files via upload

--- admin/Controller/Status.php
+++ admin/Controller/Status.php
@@ -6,9 +6,26 @@ class Status extends System {
 	public function index()	{
 		$restaurant_id = 1;
 		$floors = $this->_seats->getFloors($restaurant_id);
+		$this->floors = array();
 		foreach ($floors as $floor) {
 			$this->floors[$floor['floor_id']] = $this->_seats->getSeats($restaurant_id, $floor['floor_id']);
 		}
 		$this->setTemplate('View/Status/index.phtml');
+	}
+	public function update() {
+		$restaurant_id = isset($_POST["restaurant_id"]) ? $_POST["restaurant_id"] : "";
+		$seat = isset($_POST["seat"]) ? $_POST["seat"] : "";
+		$status = isset($_POST["status"]) ? $_POST["status"] : 0;
+		if ($restaurant_id && $seat) {
+		    $this->_seats->updateSeat($restaurant_id, $seat["floor_id"], $seat["row_no"], $seat["col_no"], $status);
+		}
+	}
+	public function updateAll() {
+		$restaurant_id = isset($_POST["restaurant_id"]) ? $_POST["restaurant_id"] : "";
+		$floor_id = isset($_POST["floor_id"]) ? $_POST["floor_id"] : "";
+		$status = isset($_POST["status"]) ? $_POST["status"] : 0;
+		if ($restaurant_id && $floor_id) {
+		    $this->_seats->updateAllSeatsByFloor($restaurant_id, $floor_id, $status);
+		}
 	}
 }
