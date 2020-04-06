49b77d956b4775fba22f7af29fa2f0cd11f904be
gladysbroce <44674409+gladysbroce@users.noreply.github.com>
2020-04-06 23:02:16 +0900

    Add files via upload

---
+++ front/Controller/User.php
@@ -0,0 +1,55 @@
+<?php
+class User extends System {
+	public function __construct() {
+		parent::__construct();
+		$this->_users = new Users();
+	}
+	public function checkLogin() {
+		$result   = 0;
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		if (!empty($username) && !empty($password)) {
+			$user = $this->_users->getUser($username);
+			if ($user && $this->_checkLoginPassword($password, $user['password'])) {
+				$_SESSION['username'] = $user['username'];
+			} else {
+				$result = 1;
+			}
+		} else {
+			$result = 2;
+		}
+		echo $result;
+	}
+	private function _checkLoginPassword($password, $db_password) {
+	    return hash('sha256', $password) === $db_password;
+	}
+	public function register() {
+		$this->setTemplate( 'View/User/index.phtml' );
+	}
+	public function add() {
+		$result    = 0;
+		$username  = $_POST['username'];
+		$password1 = $_POST['password1'];
+		$password2 = $_POST['password2'];
+		$email     = $_POST['email'];
+		if (!empty($username) && !empty($password1) && !empty($password2) && !empty($email)) {
+			if (!($this->_checkRegPassword($password1, $password2))) {
+				$result = 1;
+			} else {
+				$user = $this->_users->addUser($username, hash('sha256', $password1), $email);
+		        if (!$user) {
+					$result = 2;
+				}
+			}
+		} else {
+			$result = 3;
+		}
+		echo $result;
+	}
+	private function _checkRegPassword($password1, $password2) {
+		return ($password1 === $password2) ? true : false;
+	}
+	private function _checkUser($password, $db_password) {
+	    return hash('sha256', $password) === $db_password;
+	}
+}
--- front/Model/Users.php
+++ front/Model/Users.php
@@ -8,12 +8,28 @@ class Users {
 			FROM `user`
 			WHERE `username` = ?;
 		");
-		$stmt->bind_param("i", $username);
+		$stmt->bind_param("s", $username);
 		$stmt->execute();
 		$result = $stmt->get_result();
 		while ($row = $result->fetch_assoc()) {
 			return $row;
 		}
 		return false;
+	}
+	public function addUser($username, $password, $email) {
+		$response = false;
+		if ($username && $password && $email) {
+			$stmt = Application::DBPrepQuery( "
+			    INSERT INTO 
+			    	`user`(`username`, `password`, `email`)
+			    VALUES (?, ?, ?);
+		    ");
+			$stmt->bind_param("sss", $username, $password, $email);
+			$result = $stmt->execute();
+			if ($result) {
+				$response = true;
+			}
+		}
+		return $response;
 	}
 }
