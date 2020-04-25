258eafd31e34b19dea308c4c149b6b9fc40a3e0d
gladysbroce <44674409+gladysbroce@users.noreply.github.com>
2020-04-26 00:29:01 +0900

    Add files via upload

--- front/Controller/User.php
+++ front/Controller/User.php
@@ -12,13 +12,18 @@ class User extends System {
 		if (!empty($username) && !empty($password)) {
 			$user = $this->_users->getUser($username);
 			if ($user && $this->_checkLoginPassword($password, $user['password'])) {
+				if ($user['is_verified'] == 0){
+					$result = -1;
+				}
+				$_SESSION['user_id'] = $user['user_id'];
 				$_SESSION['username'] = $user['username'];
 				$_SESSION['restaurant_id'] = $user['restaurant_id'];
+				$_SESSION["is_verified"] = $user['is_verified'];
 			} else {
-				$result = 1;
+				$result = 1;  // Error: Invalid Password
 			}
 		} else {
-			$result = 2;
+			$result = 2; // Error: Incomplete input
 		}
 		echo $result;
 	}
@@ -34,25 +39,79 @@ class User extends System {
 		$password1 = $_POST['password1'];
 		$password2 = $_POST['password2'];
 		$email     = $_POST['email'];
-		if (!empty($username) && !empty($password1) && !empty($password2) && !empty($email)) {
-			if (!($this->_checkRegPassword($password1, $password2))) {
-				$result = 1;
-			} else {
-				$user_id = $this->_users->addUser($username, hash('sha256', $password1), $email);
-				$restaurant = $this->_restaurants->addRestaurant($user_id);
-		        if (!$restaurant) {
-					$result = 2;
-				}
-			}
+		if (empty($username) || empty($password1) || empty($password2) || empty($email)) {
+			$result = 1;  // Error: Incomplete input 
 		} else {
-			$result = 3;
+            if ($this->_checkRegUsername($username)){
+            	$result = 2;  // Error: Invalid Username
+            } else {
+				if (!($this->_checkRegPassword($password1, $password2))) {
+					$result = 3;  // Error: Invalid Password
+				} else {
+					$token = $this->_generateToken();
+					$userId = $this->_users->addUser($username, hash('sha256', $password1), $email, $token);
+					$restaurant = $this->_restaurants->addRestaurant($userId);
+			        if (!$restaurant) {
+						$result = 4;  // Error: Problem when adding to DB
+					} else {
+						$result = $this->_sendEmail($username, $email, $token);
+						if ($result != 0){
+							$result = 5;  // Error: Problem when sending email
+						}
+					}
+				}
+		    }
 		}
 		echo $result;
+	}
+	private function _checkRegUsername($username) {
+	    $user = $this->_users->getUser($username);
+	    $result = ($user) ? false : true;
 	}
 	private function _checkRegPassword($password1, $password2) {
 		return ($password1 === $password2) ? true : false;
 	}
-	private function _checkUser($password, $db_password) {
-	    return hash('sha256', $password) === $db_password;
+	private function _generateToken() {
+		$token = openssl_random_pseudo_bytes(16);
+        $token = bin2hex($token);
+        return $token;
 	}
+    private function _sendEmail($username, $recipientEmail, $token) {
+    	$result = 0;
+    	$url = "https://makakaupo.ga/user/verify?t=".$token."&user=".$username;
+        $link = '<a href="'.$url.'">'.$url.'</a>';
+		require("sendgrid-php/sendgrid-php.php");
+		$email = new \SendGrid\Mail\Mail(); 
+		$email->setFrom("noreply@makakaupo.ga", "Makakaupo");
+		$email->setSubject("Registration Verification");
+		$email->addTo($recipientEmail, $username);
+		$email->addContent(
+		    "text/html", 
+		    "Hello ".$username.",<br/><br/>".
+		    "Welcome to Makakaupo!<br/><br/>".
+		    "Please click the link below to complete your account registration.<br/>".
+            $link."<br/><br/>".
+		    "Regards,<br/>".
+		    "Makakaupo Support Team"
+		);
+		$sendgrid = new \SendGrid('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');
+		try {
+		    $response = $sendgrid->send($email);
+		} catch (Exception $e) {
+		    $result = 1;  // Error: Problem when sending email
+		}
+		return $result;
+    }
+    public function verify() {
+    	$username = isset($_GET["user"]) ? trim($_GET["user"]): "";
+        $token    = isset($_GET["t"])    ? trim($_GET["t"])   : "";
+        $response = $this->_users->verifyUser($username, $token);
+        if ($response) {
+        	$this->verifyMsg = "Your account has been successfully verified.";
+        } else {
+            $this->verifyMsg = "Your account verification has failed.";
+        }
+        $this->restaurants = $this->_restaurants->getNewRestaurants('last_modified', 'DESC', 3);
+        $this->setTemplate('View/Home/index.phtml');
+    }
 }
--- front/Model/Users.php
+++ front/Model/Users.php
@@ -3,8 +3,10 @@ class Users {
 	public function getUser($username) {
 		$stmt = Application::DBPrepQuery( "
 			SELECT 
+				`u`.`user_id`,
 				`u`.`username`,
 				`u`.`password`,
+				`u`.`is_verified`,
 				`r`.`restaurant_id`
 			FROM `user` `u`
 			INNER JOIN `restaurant` `r` 
@@ -19,18 +21,38 @@ class Users {
 		}
 		return false;
 	}
-	public function addUser($username, $password, $email) {
+	public function addUser($username, $password, $email, $token) {
+		$id = "";
 		$response = false;
-		if ($username && $password && $email) {
+        if ($username && $password && $email && $token) {
 			$stmt = Application::DBPrepQuery( "
 			    INSERT INTO 
-			    	`user`(`username`, `password`, `email`)
-			    VALUES (?, ?, ?);
+			    	`user`(`username`, `password`, `email`, `token`)
+			    VALUES (?, ?, ?, ?);
 		    ");
-			$stmt->bind_param("sss", $username, $password, $email);
+			$stmt->bind_param("ssss", $username, $password, $email, $token);
 			$result = $stmt->execute();
 			$id = $stmt->insert_id;
 		}
 		return $id;
+	}
+	public function verifyUser($username, $token) {
+		$response = false;
+		if ($username && $token) {
+			$stmt = Application::DBPrepQuery( "
+				UPDATE 
+					`user`
+				SET `is_verified` = 1
+				WHERE 
+				    `username` = ? AND
+				    `token` = ?;
+		    ");
+			$stmt->bind_param("ss", $username, $token);
+			$result = $stmt->execute();
+			if ($result) {
+				$response = true;
+			}
+		}
+		return $response;
 	}
 }
