0fe01cbd7a29b66d184b39e5ac99bece9c91a7c7
gladysbroce <44674409+gladysbroce@users.noreply.github.com>
2020-05-14 21:10:20 +0900

    Update User.php

--- front/Controller/User.php
+++ front/Controller/User.php
@@ -113,7 +113,7 @@ class User extends System {
         } else {
             $this->verifyMsg = "Your account verification has failed.";
         }
-        $this->restaurants = $this->_restaurants->getNewRestaurants('last_modified', 'DESC', 3);
+        $this->restaurants = $this->_restaurants->getNewRestaurants(3);
         $this->setTemplate('View/Home/index.phtml');
     }
 }
